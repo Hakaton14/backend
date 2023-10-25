e2ac7e9c999cd453d48187c5b50321bcf8fe02b5
Kirill Svidunovich <Altair21817@gmail.com>
2023-10-25 19:55:15 +0300

    Add TaskSerializer

--- hakaton/api/v1/serializers.py
+++ hakaton/api/v1/serializers.py
@@ -1,11 +1,30 @@
 from djoser.serializers import UserCreateSerializer
 from rest_framework.serializers import ModelSerializer
 
-from user.models import User
+from user.models import HrTasks, User
+
+
+class TaskSerializer(ModelSerializer):
+    """Сериализатор представления задач HR-специалиста."""
+
+    class Meta:
+        model = HrTasks
+        fields = (
+            'id',
+            'hr',
+            'description',
+            'date',
+            'time',
+        )
+
+    def to_representation(self, instance):
+        data: dict[str, str] = super().to_representation(instance)
+        data.pop('hr')
+        return data
 
 
 class UserRegisterSerializer(UserCreateSerializer):
-    """Сериализатор для создания нового HR-пользователя."""
+    """Сериализатор для создания нового HR-специалиста."""
 
     class Meta:
         model = User
@@ -19,7 +38,7 @@ class UserRegisterSerializer(UserCreateSerializer):
 
 
 class UserUpdateSerializer(ModelSerializer):
-    """Сериализатор представления HR пользователя."""
+    """Сериализатор представления HR-специалиста."""
 
     class Meta:
         model = User
